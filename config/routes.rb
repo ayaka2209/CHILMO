4d021bb1486fb19fc9ebc8574a84b44d32e6e1a9
ayaka tooya <na081543@icloud.com>
2022-12-01 17:56:49 +0900

    ゲストログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root "homes#index"
+  
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
 end

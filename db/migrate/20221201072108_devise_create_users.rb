ec58ef66fc4e90f73819eaa02a1fd69f4d5573cd
ayaka tooya <na081543@icloud.com>
2022-12-01 16:56:21 +0900

    deviseのフラッシュメッセージ日本語化した

--- db/migrate/20221201072108_devise_create_users.rb
+++ db/migrate/20221201072108_devise_create_users.rb
@@ -4,8 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name, null: false, limit: 50
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.boolean :admin, null: false, default: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -15,11 +17,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token

9ba93e2235a71df3c5d67f9cc2470d7eb073f7ce
ayaka tooya <na081543@icloud.com>
2022-12-02 15:59:21 +0900

    commentコントローラ編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,22 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  
   def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    # クライアント要求に応じてフォーマットを変更
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to post_path(@post) }
+      else
+        format.html { redirect_to post_path(@post), notice: '投稿できませんでした...' }
+      end
+    end
+  end
+
+  private
+  # ストロングパラメーター
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end

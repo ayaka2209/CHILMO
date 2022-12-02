e3a998a6fa9c168ced4aaa756bdd934939b5a3e6
ayaka tooya <na081543@icloud.com>
2022-12-02 15:51:43 +0900

    postとcommentを1対多の関係でアソシエーションした

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
     get 'users/sign_out', to: 'devise/sessions#destroy'
   end
+
+  resources :posts do
+    resources :comments
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_02_062918) do
+ActiveRecord::Schema.define(version: 2022_12_02_064622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.datetime "start_time", null: false
@@ -42,4 +50,5 @@ ActiveRecord::Schema.define(version: 2022_12_02_062918) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end

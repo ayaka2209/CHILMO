da41f4ef353dfadb2deb346fa67b87956dba892b
ayaka tooya <na081543@icloud.com>
2022-12-03 22:06:19 +0900

    出席一覧カレンダーからクラスごとの出席入力画面に遷移した

--- app/controllers/attendance_books_controller.rb
+++ app/controllers/attendance_books_controller.rb
@@ -11,17 +11,17 @@ class AttendanceBooksController < ApplicationController
   def create
     # @attendancebook = Attendancebook.create(attendancebook_params)
     # "attendancebook"=>{"13_presence_or_absence"=>"1", "team_id"=>"10"}
-    team_id = params[:attendancebook][:team_id].to_i
+    team_id = params[:attendance_book][:team_id].to_i
     team = Team.find(team_id)
     # team.kid_ids #=> [13, 14] 
     # byebug
-    unless params[:attendancebook].values.include?("")
+    unless params[:attendance_book].values.include?("")
       team.kid_ids.each do |kid_id|
-        attendance = params[:attendancebook]["#{kid_id}_attendance"].to_i
+        attendance = params[:attendance_book]["#{kid_id}_attendance"].to_i
         AttendanceBook.create(kid_id: kid_id, presence_or_absence: attendance, start_time: Date.today)
       end
       # ContactMailer.contact_mail(@contact).deliver
-      redirect_to attendancebooks_path, notice: "出席確認できました！"
+      redirect_to attendance_books_path, notice: "出席確認できました！"
     else
       redirect_to team_path(id: team_id), notice: "もう一度確認してください！"
     end

56b1b64870bc321a68b1be92eb33a5ea252df79b
ayaka tooya <na081543@icloud.com>
2022-12-07 11:37:00 +0900

    postテーブルにnameカラムを追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_06_085022) do
+ActiveRecord::Schema.define(version: 2022_12_07_023546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_12_06_085022) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.integer "status"
+    t.string "name"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 

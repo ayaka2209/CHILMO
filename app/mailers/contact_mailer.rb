c1a58346c88dab7702e0f3f3315bd80e766bed30
ayaka tooya <na081543@icloud.com>
2022-12-12 16:02:09 +0900

    インデント確認

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,10 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(team)
     @team = team
+    # @team.kids.each do |kid|
 
+      # mail to: kid.pearent.email, subject: "#{@team.name}の出欠確認のお知らせ"
+    # end
     mail to: "test@test.com", subject: "#{@team.name}の出欠確認のお知らせ"
   end
 end

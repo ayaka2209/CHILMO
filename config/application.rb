f3ae69a5f13b7392234027e42c4e6e1806cc855f
ayaka tooya <na081543@icloud.com>
2022-12-14 13:32:30 +0900

    cssを変更

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module CHILMO
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    
     config.load_defaults 6.1
     config.i18n.default_locale = :ja 
     config.time_zone = 'Tokyo'

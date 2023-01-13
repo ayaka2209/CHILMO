0bf60942382add50e5a48755b8cec61dfb0c3ad3
ayaka tooya <na081543@icloud.com>
2023-01-13 09:17:08 +0900

    endつけ忘れを訂正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -24,3 +24,4 @@ CarrierWave.configure do |config|
       config.fog_directory = 'chilmo-development'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chilmo-development'
   end
+end
